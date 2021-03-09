b7d45e6bbce1b4ad23015d08a8ab96102ef4b6d1
ajoib <77057949+ajoib@users.noreply.github.com>
2021-03-09 16:01:49 +0500

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,11 @@
 $(function() { 
-    $('.menu_btn').click(function(){ 
-        
+    $('.menu_open').click(function(){ 
         $('.menu_ul').toggleClass('active');
      });
  })
  
- 
+ $(function() { 
+    $('.menu_close').click(function(){ 
+        $('.menu_ul').toggleClass('active');
+     });
+ })

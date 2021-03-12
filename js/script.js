1558d5414fac8349c9b8cfa391cec27d9eba22f1
ajoib <77057949+ajoib@users.noreply.github.com>
2021-03-12 20:41:57 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() { 
+    $('.menu_btn').click(function(){ 
+        $('menu_ul').show('active');
+        $('.menu_ul').toggleClass('active');
+     });
+ })
+ 
+ $(function() { 
+    $('.menu_close').click(function(){ 
+        $('menu_ul').show('active');
+        $('.menu_ul').toggleClass('active');
+     });
+ })
